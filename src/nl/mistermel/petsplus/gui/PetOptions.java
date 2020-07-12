5a32202ee4c515846d713d3d883e25e997c44247
Melle Moerkerk <melle@mistermel.tech>
2020-07-12 15:26:14 +0200

    Added remove option to rightclick-menu

--- src/nl/mistermel/petsplus/gui/PetOptions.java
+++ src/nl/mistermel/petsplus/gui/PetOptions.java
@@ -28,8 +28,14 @@ public class PetOptions extends Gui {
 		rideMeta.setDisplayName(PetsPlus.getInstance().getConfigManager().getGuiSetting("ride-item"));
 		ride.setItemMeta(rideMeta);
 		
+		ItemStack remove = new ItemStack(Material.BARRIER);
+		ItemMeta removeMeta = remove.getItemMeta();
+		removeMeta.setDisplayName(PetsPlus.getInstance().getConfigManager().getGuiSetting("remove-pet-item"));
+		remove.setItemMeta(removeMeta);
+		
 		inv.setItem(11, sound);
 		inv.setItem(13, ride);
+		inv.setItem(15, remove);
 	}
 
 	@Override
@@ -39,10 +45,18 @@ public class PetOptions extends Gui {
 			for(Player p2 : Bukkit.getOnlinePlayers()) {
 				p2.playSound(PetsPlus.getInstance().getPet(p).getEntity().getLocation(), PetsPlus.getInstance().getPet(p).getSound(), SoundCategory.AMBIENT, 1f, 1f);
 			}
+			return;
 		}
 		if(item.getType() == Material.SADDLE) {
 			p.closeInventory();
 			PetsPlus.getInstance().getPet(p).getEntity().addPassenger(p);
+			return;
+		}
+		if(item.getType() == Material.BARRIER) {
+			p.sendMessage(PetsPlus.getInstance().getConfigManager().getPrefix() + PetsPlus.getInstance().getConfigManager().getMessage("removed-pet").replaceAll("%pet-name%", PetsPlus.getInstance().getPet(p).getEntity().getType().getName()));
+			p.closeInventory();
+			PetsPlus.getInstance().removePet(p);
+			return;
 		}
 	}
 	
